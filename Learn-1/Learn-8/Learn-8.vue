aa97495444cbb10107e3ffd046dd54e004a6fb8d
mm2m123 <eminem13@126.com>
2020-12-07 17:38:06 +0800

    learn

---
+++ Learn-1/Learn-8/Learn-8.vue
@@ -0,0 +1,20 @@
+<body>
+    <div id="hello-vue" class="m-3 p-3 border border-success">
+        {{ x + y }}
+    </div>
+    <script>
+Vue.createApp({
+  /* options */
+  data() {
+    return {
+      x: 10,
+      y: 20,
+    };
+  },
+  created() {
+    // 组件生成时
+    console.log("x + y is: " + (this.x + this.y));
+  },
+}).mount("#hello-vue");
+</script>
+</body>
